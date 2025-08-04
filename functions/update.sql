3e0a95fd39b0c936bab3649564f93897348df2f2
F10-silva <igor.f.silva14@aluno.senai.br>
2025-08-04 10:17:53 -0300

    Create update.sql

---
+++ functions/update.sql
@@ -0,0 +1,9 @@
+update funcionarios
+set salario = 7500
+where id = 5;
+-- ele  atualiza o salário do funcionario de id 5
+
+update projetos
+set concluido = true
+where id = 5;
+-- esse atualiza a conclusão do projeto de id 5
